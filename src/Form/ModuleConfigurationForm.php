ad08462db69b7d12e8c4c1dfcc3abf7e98534b84
AnantaGoswami <57352149+AnantaGoswami@users.noreply.github.com>
2019-11-11 16:07:52 +0530

    Create ModuleConfigurationForm.php

---
+++ src/Form/ModuleConfigurationForm.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\node_json_data\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+
+class ModuleConfigurationForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'api_key_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'api_key.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('api_key.settings');
+
+    $form['api_key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Enter Api key'),
+      '#default_value' => $config->get('api_key'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Retrieve the configuration.
+    $this->configFactory->getEditable('api_key.settings')
+      // Set the submitted configuration setting.
+      ->set('api_key', $form_state->getValue('api_key'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
